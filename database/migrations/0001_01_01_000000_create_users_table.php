f565aea73c8976352523dc13607324a7463d61c6
wildhanry <garpu3600.com>
2025-11-17 17:14:56 +0700

    Move unique constraint to separate statement at end of users table definition

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,12 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 255);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role', 50)->default('pendaftaran');
             $table->rememberToken();
             $table->timestamps();
+            $table->unique('email');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
